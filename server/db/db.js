e10a0ebb829fc2591e6dfbacfdbce4558060de03
Mark Eggensperger <markeggensperger@gmail.com>
2021-03-27 15:08:28 -0400

    updating json file

--- server/db/db.js
+++ server/db/db.js
@@ -3,6 +3,28 @@ const Sequelize = require('sequelize');
 const dbName = 'mrbartender';
 console.log(`Opening database connection to ${dbName}`);
 
+const databaseName =
+  pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '');
+
+let config;
+
+if (process.env.DATABASE_URL) {
+  config = {
+    logging: false,
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  };
+} else {
+  config = {
+    logging: false,
+  };
+}
+
 const databaseUrl =
   process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`;
 
